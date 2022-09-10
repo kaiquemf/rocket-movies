fe10f024d03009d8c0d32161158d8e947f566354
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-09-10 14:27:24 -0300

    add global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,15 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  :root{
+    font-size: 62.5%;
+    font-family: 'Roboto Slab', serif;
+  }
+  body{
     background: ${({ theme }) => theme.COLORS.BACKGROUND_DARK};
   }
 `
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,20 @@
 export default {
   COLORS:{
 
-    BACKGROUND_DARK:'hsla(0, 0%, 7%, 1)' ,
+    BACKGROUND_DARK:'	hsla(260, 5%, 11%, 1)',
+    BACKGROUND_LIGHT:'hsla(260, 0%, 98%, 1)',
+
+    HEADER_BORDER:'hsla(255, 9%, 25%, 1)',
+    INPUT_BG:'hsla(255, 5%, 15%, 1)',
+
+    HARD_BLUE:'hsla(225, 100%, 76%, 1)',
+    SOFT_BLUE:'hsla(225, 100%, 76%, .5)',
+
+    WHITE_900:'hsla(255, 100%, 100%, 1)',
+    WHITE_800:'hsla(25, 35%, 93%, 1)',
+
+    GRAY_900:'hsla(0, 0%, 90%, 1)',
+    GRAY_800:'hsla(270, 5%, 58%, 1)',
+    GRAY_700:'hsla(30, 4%, 58%, 1)',
   }
 }
