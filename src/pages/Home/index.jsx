434e5605cf7e4837d670f570443e998573c0a31a
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-09-10 16:33:14 -0300

    add button component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,16 @@
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { FiPlus } from "react-icons/fi";
 
 export function Home(){
   return(
     <Container>
       <Header />
+      <Content>
+        <h1>My Movies</h1>
+        <Button icon={FiPlus} title="Add new movie"/>
+      </Content>
     </Container>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+`
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 5rem 0 0;
+  padding: 0 12.3rem;
+  > h1{
+    font-size: 3.2rem;
+    line-height: 4.2rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.COLORS.WHITE_900};
+    white-space: nowrap;
+  }
+  > button{
+    cursor: pointer;
+    width: 20rem;
+
+    transition: all 200ms ease-in-out;
+  }
+  button:hover{
+    filter: opacity(0.8);
+  }
 `
