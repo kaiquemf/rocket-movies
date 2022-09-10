b28388b94c967fd0c75ccd2d0d9538371f22cdda
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-09-10 16:08:27 -0300

    update header components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,20 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
+import { FiSearch } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import { Input } from "../Input";
 
 export function Header(){
   return(
     <Container>
       <h1>RocketMovies</h1>
-      <Input />
+      <Input placeholder="Search Title" icon={FiSearch}/>
+      <Profile>
+        <div className="text-content">
+        <strong>Kaique Martins</strong>
+        <button>logout</button>
+        </div>
+        <img src="https://github.com/kaiquemf.png" alt="Profile Image" />
+      </Profile>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -8,6 +8,8 @@ export const Container = styled.header`
   align-items: center;
   justify-content: space-between;
   padding: 0 12.3rem;
+  gap: 6.4rem;
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.HEADER_BORDER};
 
   > h1{
     font-size: 2.4rem;
@@ -15,5 +17,40 @@ export const Container = styled.header`
     font-weight: 700;
 
     color: ${({ theme }) => theme.COLORS.HARD_BLUE};
+  }
+`
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: .8rem;
+
+  > .text-content{
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+
+    strong{
+      font-size: 1.4rem;
+      line-height: 1.8rem;
+      font-weight: 700;
+      white-space: nowrap;
+
+      color: ${({ theme }) => theme.COLORS.WHITE_800};
+    }
+    button{
+      cursor: pointer;
+      background: none;
+      border: none;
+      font-family: 'Roboto Slab', serif;
+      font-size: 1.4rem;
+      line-height: 1.8rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_800};
+    }
+  }
+  > img{
+    width: 6.4rem;
+    height: 6.4rem;
+    border-radius: 50%;
+    border: 1px solid ${({ theme }) => theme.COLORS.HEADER_BORDER};
   }
 `
