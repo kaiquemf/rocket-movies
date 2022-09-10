3a8f111576ec54729e783e6dd0eb43bcd119ea65
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-09-10 16:08:46 -0300

    update Input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,19 +2,34 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 5.6rem;
-  
-  input{
-    width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: .8rem;
+  border-radius: 1rem;
+
+  background: ${({ theme }) => theme.COLORS.INPUT_BG};
+  color: ${({ theme }) => theme.COLORS.GRAY_800};
+
+  textarea:focus, input:focus{
+    outline: none;
+  }
+
+  > input{
     height: 5.6rem;
-    border: none;
-    border-radius: 1rem;
-    padding: 1.9rem 2.4rem;
-    
-    background: ${({ theme }) => theme.COLORS.INPUT_BG};
-    
+    width: 100%;
+    padding: 1.6rem;
+    font-family: 'Roboto Slab', serif;
+
+    color: ${({ theme }) => theme.COLORS.WHITE_900};
+    border: 0;
+    background: transparent;
+
     &::placeholder{
-      color: ${({ theme }) => theme.COLORS.GRAY_800}
+      color: ${({ theme }) => theme.COLORS.GRAY_800};
     }
+  }
+  > svg{
+    margin: 1.6rem 0 1.6rem 1.6rem;
   }
 `
