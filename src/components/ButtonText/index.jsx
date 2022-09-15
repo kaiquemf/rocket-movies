7101610de762ef5cdf8b3472bbc6e8eadee0c815
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-09-15 18:40:30 -0300

    import button text

---
+++ src/components/ButtonText/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './style'
+
+export function ButtonText({title, isActive = false, ...rest}){
+  return(
+    <Container {...rest} type= "button" isActive={ isActive }>
+      {title}
+    </Container>
+  )
+}
---
+++ src/components/ButtonText/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  font-family: 'Roboto Slab', serif;
+  font-size: 1.6rem;
+  color: ${({ theme, isActive }) => isActive ? theme.COLORS.HARD_BLUE : theme.COLORS.GRAY_800};
+
+  cursor: pointer;
+  background: none;
+  border: none;
+  
+  transition: all 200ms ease-in-out;
+  &:hover{
+    color: ${({ theme }) => theme.COLORS.HARD_BLUE}
+  }
+`;
