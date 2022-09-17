166f958163b6ec9bee849f574cf5f8da3aa29492
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2022-09-17 18:55:04 -0300

    create Section component

---
+++ src/components/Section/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  margin: 2.8rem 0;
+
+  > h2{
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: ${({ theme }) => theme.COLORS.INPUT_BG};
+    
+    padding-bottom: 1.6rem;
+    margin-bottom: 2.4rem;
+    color: ${({ theme }) => theme.COLORS.GRAY_700};
+    font-size: 2rem;
+    font-weight: 400;
+  }
+`
